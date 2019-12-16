60292db31e1fa796a69bade645dea9a1574c438b
BeauTaapken <beautaapken@me.com>
2019-12-16 16:59:31 +0100

    * Added physicshandle to the player object * Added the physicshandle to the code so it gets filled

--- Source/BuildingEscape/Grabber.cpp
+++ Source/BuildingEscape/Grabber.cpp
@@ -26,7 +26,18 @@ void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Error, TEXT("Grabber reporting for duty!"));
+	UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty!"));
+
+	//Look for attached physics handle
+	physicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+	if(physicsHandle)
+	{
+		//physicsHandle is found
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("No physicshandle has been found on object %s"), *GetOwner()->GetName())
+	}
 }
 
 
